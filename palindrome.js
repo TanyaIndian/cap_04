745f09965a87f4b36a89d3919d1f01f599d7f158
TanyaIndian <tanyaindian24@gmail.com>
2023-10-05 20:12:42 +0530

    added palindrome file

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let name = "naman";
+let bag = ""; 
+for(let i = name.length - 1; i >=0; i--)
+{
+    bag += name[i]
+}
+
+if(name == bag)
+{
+    console.log("Panlindrome");
+}
+else{
+    console.log("Not palindrome");
+}
